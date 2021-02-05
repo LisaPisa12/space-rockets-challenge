0b92036922e5fda0c4f6ef4a3b70575a2ec90449
LisaPisa12 <esarq001@fiu.edu>
2021-02-05 18:48:38 +0100

    added global provider

--- src/components/app.js
+++ src/components/app.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Flex, Text } from "@chakra-ui/core";
-
+import { ChakraProvider } from "@chakra-ui/react";
 import Launches from "./launches";
 import Launch from "./launch";
 import Home from "./home";
 import LaunchPads from "./launch-pads";
 import LaunchPad from "./launch-pad";
-///test
+import GlobalProvider from "../Context/GlobalState";
+
 export default function App() {
   return (
-    <div>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/launches" element={<Launches />} />
-        <Route path="/launches/:launchId" element={<Launch />} />
-        <Route path="/launch-pads" element={<LaunchPads />} />
-        <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
-      </Routes>
-    </div>
+    <GlobalProvider>
+      <ChakraProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/launches" element={<Launches />} />
+          <Route path="/launches/:launchId" element={<Launch />} />
+          <Route path="/launch-pads" element={<LaunchPads />} />
+          <Route path="/launch-pads/:launchPadId" element={<LaunchPad />} />
+        </Routes>
+      </ChakraProvider>
+    </GlobalProvider>
   );
 }
 
